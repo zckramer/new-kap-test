7f49116dd8ddc68b599de12d7664c87f62f85fda
Zachary Kramer <zckramer83@gmail.com>
2020-11-22 15:36:48 -0500

    added a company profile card, added logic to color text

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import axios from 'axios';
 import CompanyCard from './components/CompanyCard';
 
 function App() {
-  const [inputValue, setInputValue] = useState();
-  const [responseData, setResponseData] = useState();
+  const [inputValue, setInputValue] = useState(); // Search input state
+
+  const [profileData, setProfileData] = useState(); //  profile 
+  const [isTickerDataLoaded, setIsTickerDataLoaded] = useState(false);
 
   const BASE_URL = "https://financialmodelingprep.com/api/v3/profile/";
   const apiKey = "?apikey=f3cadaf0569807e5dd47e435a6d8c5a6"
@@ -13,13 +15,18 @@ function App() {
   function handleSubmit (event) {
     event.preventDefault();
     axios.get(BASE_URL + inputValue + apiKey)
-      .then (res => setResponseData(res.data[0]))
-      .catch()
-      .then(console.log(responseData))
+      .then (res => handleProfileSearch(res.data))
+      .catch();
   }
   
   function handleInputChange (event) {
     setInputValue(event.target.value);
+    setIsTickerDataLoaded(false);
+  }
+
+  function handleProfileSearch (responseData) {
+    setProfileData(responseData);
+    setIsTickerDataLoaded(true);
   }
 
   return (
@@ -29,14 +36,13 @@ function App() {
         <label>Search for a company profile: 
           <input 
             className="input" 
-            placeholder=" (i.e. AAPL)" 
+            placeholder=" (i.e. AAPL or TIGR)" 
             onChange={(event)=>handleInputChange(event)}
           />
         </label>
         <button type="submit">Submit</button>
       </form>
-      <div>input value : {inputValue}</div>
-      <CompanyCard data={responseData}/>
+      {isTickerDataLoaded ? profileData.map(profile=> <CompanyCard data={profile}/>) : "Enter a company trade symbol above."}
     </div>
   );
 }
--- src/components/CompanyCard.jsx
+++ src/components/CompanyCard.jsx
@@ -1,10 +1,55 @@
 const CompanyCard = (props) => {
+    const profile = props.data;
     return (
         <div className="Company-Card">
             <div className="Company-Card__Header">
-                <h3 className="Company-Card__Company-Name">{props.companyName}</h3>
-                <img className="Company-Card__Company-Logo" src={props.image} />
-                
+                <h3 className="Company-Card__Company-Name">{profile.companyName}</h3>
+                <img className="Company-Card__Company-Logo" src={profile.image} />    
+            </div>
+            <div className="Company-Card__Body">
+                <div className='Card-Data'>
+                    <span id="Card-Data__Name" >{<a href={profile.website} >{profile.companyName}</a>}</span>
+                    <hr />
+                    <span id="Card-Data__Symbol" >{profile.symbol}</span>
+                     <hr />
+                    <span id="Card-Data__CEO" >{profile.ceo} </span>
+                    <hr />
+                    <span id="Card-Data__Exchange" >{profile.exchange} </span>
+                    <hr />
+                    <span id="Card-Data__Industry" >{profile.industry} </span>
+                    <hr />
+                    <span id="Card-Data__Sector" >{profile.sector} </span>
+                    <hr />
+                    <span 
+                    id="Card-Data__Changes-Percent" 
+                    style={profile.changesPercent && profile.changesPercent.includes("-") ? 
+                    {color: 'red'} : {color: 'green'}}>
+                    {profile.changesPercent}
+                    </span>
+                    <hr />
+                    <span 
+                    id="Card-Data__Changes"
+                    style={profile.changes < 0 ? 
+                    {color: 'red'} : {color: 'green'}}>
+                    {profile.changes}
+                    </span>
+                <hr />
+                <li>
+                    <span id="Card-Data__Price" >{profile.price} </span>
+                </li>
+                <hr />
+                <li>
+                    <span id="Card-Data__Beta" >{profile.beta} </span>
+                </li>
+                <hr />
+                <li>
+                    <span id="Card-Data__Volume-Average" >{profile.volAvg} </span>
+                </li>
+                <hr />
+                <li>
+                    <div id="Card-Data__Description" >{profile.description}</div>
+                </li>
+		        </div>            
             </div>
         </div>
     )
